7689024206551250d47c0ea757fdcf5e39dde918
Iipal <malkoleyplay@gmail.com>
2020-02-10 21:11:37 +0200

    fixed and improvs in malloc

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,64 +1,71 @@
+#define _GNU_SOURCE
+
 #include "libmalloc.h"
 
 #define LIBM_INTERNAL
 #include "libm_internal.h"
 #undef LIBM_INTERNAL
 
-void	*__mlast = NULL;
+void	*__mend = NULL;
 void	*__mstart = NULL;
 
-inline void	malloc_init(void) {
+static inline void	malloc_init(void) {
 	__mstart = sbrk(0);
-	if ((void*)-1 == __mstart)
-		err(errno, "");
-	__mlast = __mstart;
+	__mend = __mstart;
+	assert((void*)-1 != (__mstart = sbrk(0)));
 }
 
-inline void	*find_best_free_block(const mblk_t __size) {
+static inline void	*find_best_free_block(const mblk_t __size) {
 	void	*__iptr = __mstart;
 	void	*__bestptr = NULL;
-	mblk_t	__bestsize = __mlast - __mstart;
+	mblk_t	__bestsize = ~0UL;
+	mblk_t	__isize = 0UL;
 
-	while (__iptr < __mlast) {
-		const mblk_t __isize = __mblk_get_size(__iptr);
-
+	while (__iptr < __mend) {
+		__isize = __mblk_get_size(__iptr);
 		if (__mblk_is_free(__iptr)
 		&& __isize >= __size
-		&& __isize - __size < __bestsize) {
-			__bestsize = __isize - __size;
+		&& __isize <= __bestsize) {
+			__bestsize = __isize;
 			__bestptr = __iptr;
 		}
-		__iptr += __isize + __mblkt_bd_size;
+		__iptr += __mblkt_iter(__isize);
 	}
 	if (__bestptr) {
 		__mblk_set_size(__bestptr, __size);
 		__mblk_set_free(__bestptr, __size);
+		if (__bestsize > __size) {
+			__mblk_set_size(__bestptr + __mblkt_iter(__size),
+				__bestsize - __size);
+			__mblk_unset_free(__bestptr + __mblkt_iter(__size),
+				__bestsize - __size);
+		}
 	}
 	return __bestptr;
 }
 
-inline void	*new_block(const mblk_t __size) {
+static inline void	*new_block(const mblk_t __size) {
 	const mblk_t	__asize = __size + __mblkt_bd_size;
 	void			*out = NULL;
 
 	assert((void*)-1 != (out = sbrk(__asize)));
-	__mlast += __asize;
+	__mend += __asize;
 	__mblk_set_size(out, __size);
 	__mblk_set_free(out, __size);
 	return out;
 }
 
 void	*malloc(size_t size) {
-	mblk_t	__size = size;
-	void	*out = NULL;
+	const mblk_t	__size = (size % sizeof(void*))
+							? __mblk_align_size(size)
+							: size;
+	void			*out = NULL;
 
-	if (__size % sizeof(void*))
-		__size += (~size & __mblk_align_bits) + 1UL;
-	if (!__mstart && !__mlast)
+	if (!__mstart && !__mend)
 		malloc_init();
-	if (__mstart == __mlast)
-		out = new_block(__size);
-	else
+	if (__mstart != __mend)
 		out = find_best_free_block(__size);
-	return out + __mblkt_size;
+	if (!out)
+		out = new_block(__size);
+	return __mblk_get_ptr(out);
 }
