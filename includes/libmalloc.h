ba8dee0c8e0bd85617c9da39208e74cc4db678eb
Iipal <malkoleyplay@gmail.com>
2020-02-10 21:07:47 +0200

    update:
    added calloc and realloc
    fixed mtrace commentary\documentation
    added special for mtrace \param flags macroses

--- includes/libmalloc.h
+++ includes/libmalloc.h
@@ -1,11 +1,36 @@
 #ifndef LIBMALLOC_H
 # define LIBMALLOC_H
 
-# define _GNU_SOURCE
-
-# include <sys/types.h>
+# include <stddef.h>
+# include <features.h>
 
 extern void	*malloc(size_t size);
 extern void	free(void *restrict ptr);
+extern void	*calloc(size_t nmemb, size_t size);
+extern void	*realloc(void *ptr, size_t size);
+
+# ifdef __USE_MISC
+
+# define libm_mtrace_not_freed 1
+# define libm_mtrace_freed     2
+# define libm_mtrace_all       4
+# define libm_mtrace_none      0
+
+/*
+	\param n_blocks is a limit to print\counter allocated blocks.
+	If \param flags are non-zero value - debug info will be printed.
+		\param flags specification:
+			libm_mtrace_not_freed(1) - print info only about not freed blocks.
+			libm_mtrace_freed(2)     - print info only about freed blocks.
+			libm_mtrace_all(4)       - print both of the above info.
+			libm_mtrace_none(0)      - doesn't print anything.
+				Just will count allocated blocks.
+
+	\return value is a counter of allocated blocks
+	 and not greater than \param n_blocks.
+*/
+extern size_t	mtrace(size_t n_blocks, int flags);
+
+#endif
 
 #endif
