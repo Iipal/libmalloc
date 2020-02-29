cb67c50d7662f007258bd36d9ef5b5653344adbd
Iipal <malkoleyplay@gmail.com>
2020-02-29 23:31:07 +0200

    from scrath

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -6,20 +6,8 @@
 #include "libm_internal.h"
 #undef LIBM_INTERNAL
 
-// with optimization, this code will transform into libc-memset call.
-static inline void	*_mbzero(void *ptr, const mblk_value_t size) {
-	unsigned char	*__iptr = (unsigned char*)ptr;
-	mblk_value_t	__isize = size;
-
-	while (__isize--)
-		__iptr[__isize] = 0;
-	return ptr;
-}
-
 inline void	*calloc(size_t nmemb, size_t size) {
-	const mblk_value_t	__size = __mblk_align_size(nmemb * size);
-	void	*out = NULL;
+	void *restrict	out = malloc(nmemb * size);
 
-	out = malloc(__size);
-	return out ? _mbzero(out, __size) : out;
+	return out ? _mbzero(out, nmemb * size) : out;
 }
--- srcs/realloc.c
+++ srcs/realloc.c
@@ -4,42 +4,8 @@
 #include "libm_internal.h"
 #undef LIBM_INTERNAL
 
-// with optimization, this code will transform into libc-memcpy call.
-static inline void	*_mmemcpy(void *restrict dst,
-						const void *restrict src,
-						size_t n) {
-	unsigned char	*__dptr = (unsigned char*)dst;
-	const unsigned char	*__sptr = (unsigned char*)src;
-	mblk_value_t	__isize = n;
-
-	while (__isize--)
-		__dptr[__isize] = __sptr[__isize];
-	return dst;
-}
-
-inline void	*realloc(void *ptr, size_t size)
-{
-	const mblk_value_t	__newsize = __mblk_align_size(size);
-
-	if (!ptr) {
-		return malloc(__newsize);
-	} else if (!!ptr && !size) {
-		free(ptr);
-		return NULL;
-	}
-
-	void	*__ptr = ptr - __mblkt_size;
-	const mblk_value_t	__ptrsize = __mblk_get_size(__ptr);
-
-	if (__ptrsize >= __newsize) {
-		return ptr;
-	} else {
-		void	*out = malloc(__newsize);
-
-		if (out) {
-			_mmemcpy(out, ptr, __ptrsize);
-			free(ptr);
-		}
-		return out;
-	}
+inline void	*realloc(void *ptr, size_t size) {
+	(void)size;
+	free(ptr);
+	return NULL;
 }
