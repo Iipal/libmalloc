22249928ead3e860e0d2dc7330d97f49ba1c8fee
Iipal <malkoleyplay@gmail.com>
2020-03-01 23:12:57 +0200

    using glibc-bzero()

--- srcs/calloc.c
+++ srcs/calloc.c
@@ -6,21 +6,12 @@
 #include "libm_internal.h"
 #undef LIBM_INTERNAL
 
-// with optimization, this code will transform into libc-memset call.
-static inline void	*_mbzero(void *ptr, size_t size);
-
 void	*calloc(size_t nmemb, size_t size) {
 	size_t	__align_size = __mblk_align_size(nmemb * size);
 	void	*out = malloc(__align_size);
 
-	return out ? _mbzero(out, __align_size) : out;
-}
-
-static inline void	*_mbzero(void *ptr, size_t size) {
-	unsigned char	*__iptr = (unsigned char*)ptr;
-	size_t	__isize = size;
-
-	while (__isize--)
-		__iptr[__isize] = 0;
-	return ptr;
+	if (out) {
+		bzero(out, __align_size);
+	}
+	return out;
 }
